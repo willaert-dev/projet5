a1e59cc035fb353860454da3bed103d3961bc776
willaert-dev <125659755+willaert-dev@users.noreply.github.com>
2023-10-13 11:20:14 +0200

    avancé javaScript sur les bullets point

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,68 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 const arrow_left = document.querySelector(".arrow_left");
 const arrow_right = document.querySelector(".arrow_right");
 
-arrow_left.addEventListener("click", console.log("gauche"));
-arrow_right.addEventListener("click", bulletRight);
-
+arrow_left.addEventListener("click", function () {
+  moveSlider(-1);
+});
+arrow_right.addEventListener("click", function () {
+  moveSlider(1);
+});
+function moveSlider(slideMove) {
+  console.log(slideMove);
+}
 const nb_points = slides.length;
 console.log(nb_points);
-
 
 let dots = document.querySelector(".dots");
 
 for (let i = 0; i < nb_points; i++) {
-    const point = document.createElement("div");
-    dots.appendChild(point);
-    point.classList.add("dot");
+  const point = document.createElement("div");  
+  point.classList.add("dot");
+  dots.appendChild(point);
 }
 
 tableau_point = document.querySelectorAll(".dot");
 tableau_point[0].classList.add("dot_selected");
 
 function bulletRight() {
-	indice = 0; 
- for (let index = 0; index < nb_points; index++) {
-	compteur = tableau_point[index].classList;
- if (compteur.contains("dot_selected")) {
-	indice = index;	
- }
+  indice = 0;
+  for (let index = 0; index < nb_points; index++) {
+    nomClass = tableau_point[index].classList;
+    if (nomClass.contains("dot_selected")) {
+      indice = index;
+    }
+  }
+}
 
- tableau_point[indice].classList.remove("dot_selected")
-}	
- }	
+function bulletLeft() {
+  for (let index = 0; index < nb_points; index++) {
+    nomClass = tableau_point[index].classlist;
+    if (nomClass.contains("dot_selected")) {
+      index = index;
+    }
+  }
+}
+function getIndexSlider(){
+	let indexSlider;
 
+	return indexSlider;
+}
