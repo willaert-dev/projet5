b20fa4c4f3a8b74cd98ed730f4bd0d0353d89f8d
willaert-dev <125659755+willaert-dev@users.noreply.github.com>
2023-10-25 12:22:44 +0200

    essai  image slider

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,77 @@
-git 
+const slides = [
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+const arrow_left = document.querySelector(".arrow_left");
+const arrow_right = document.querySelector(".arrow_right");
+
+arrow_left.addEventListener("click", bulletSliderLeft);
+arrow_right.addEventListener("click", bulletSliderRight);
+
+const nb_points = slides.length;
+
+
+const bannerImage = document.querySelector("banner-img")
+//bannerImage.src = "./assets/images/slideshow/slide1" = slides[0].image;
+
+//const bannerPara = document.querySelector("#banner-p");
+//bannerPara.innerHTML = slides[1].tagLine;
+
+
+
+let dots = document.querySelector(".dots");
+for (let i = 0; i < nb_points; i++) {
+  const point = document.createElement("div");
+  dots.appendChild(point);
+  point.classList.add("dot");
+}
+
+tableau_point = document.querySelectorAll(".dot");
+tableau_point[0].classList.add("dot_selected");
+
+function bulletSliderRight() {
+  let indice = 0;
+  for (let index = 0; index < nb_points; index++) {
+    compteur = tableau_point[index].classList;
+    if (compteur.contains("dot_selected")) {
+      indice = index ;
+    };
+  }
+  console.log(indice);  
+  tableau_point[indice].classList.remove("dot_selected");
+  if (indice==3) { indice=0; }   else { indice++};
+  tableau_point[indice].classList.add("dot_selected");
+  bannerImage.src = "./assets/images/slideshow/" + slides[indice].image;
+  bannerPara.innerHTML = slides[indice].tagLine; 
+}
+function bulletSliderLeft() {
+  let indice = 0;
+  for (let index = 0; index < nb_points; index++) {
+    compteur = tableau_point[index].classList;
+    if (compteur.contains("dot_selected")) {
+      indice=index;
+    };
+  }
+  console.log(indice);  
+  tableau_point[indice].classList.remove("dot_selected");
+  if (indice==0) { indice=3; }   else { indice--};
+  tableau_point[indice].classList.add("dot_selected");
+
+  bannerImage.src = "./assets/images/slideshow/" + slides[indice].image;
+  bannerPara.innerHTML = slides[indice].tagLine;
+}
