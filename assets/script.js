3fbc4aeca430ef8a1b1387e90e40fb99ff1791bc
willaert-dev <125659755+willaert-dev@users.noreply.github.com>
2023-10-26 12:13:38 +0200

    fin de projet 5

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,7 @@ const slides = [
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
+// intégration des flêches sur bas du slider avec l'écoute au click sur chacune des flêches
 const arrow_left = document.querySelector(".arrow_left");
 const arrow_right = document.querySelector(".arrow_right");
 
@@ -24,16 +25,17 @@ arrow_left.addEventListener("click", bulletSliderLeft);
 arrow_right.addEventListener("click", bulletSliderRight);
 
 const nb_points = slides.length;
+console.log(nb_points)
+
+// récupération et intégration des images correspondant aux textes  dans le slide 
+const bannerImage = document.querySelector(".banner-img");
+bannerImage.src ="./assets/images/slideshow/" + slides[0].image;
 
 
-const bannerImage = document.querySelector("banner-img")
-//bannerImage.src = "./assets/images/slideshow/slide1" = slides[0].image;
+const bannerParaf = document.querySelector("#banner P");
+bannerText.innerHTML = slides[0].tagLine;
 
-//const bannerPara = document.querySelector("#banner-p");
-//bannerPara.innerHTML = slides[1].tagLine;
-
-
-
+// créations des bullets point et de leur intégration avec le changement couleur en fonction du défilement
 let dots = document.querySelector(".dots");
 for (let i = 0; i < nb_points; i++) {
   const point = document.createElement("div");
@@ -44,6 +46,7 @@ for (let i = 0; i < nb_points; i++) {
 tableau_point = document.querySelectorAll(".dot");
 tableau_point[0].classList.add("dot_selected");
 
+// création 
 function bulletSliderRight() {
   let indice = 0;
   for (let index = 0; index < nb_points; index++) {
@@ -56,9 +59,11 @@ function bulletSliderRight() {
   tableau_point[indice].classList.remove("dot_selected");
   if (indice==3) { indice=0; }   else { indice++};
   tableau_point[indice].classList.add("dot_selected");
+
   bannerImage.src = "./assets/images/slideshow/" + slides[indice].image;
-  bannerPara.innerHTML = slides[indice].tagLine; 
+  bannerText.innerHTML = slides[indice].tagLine; 
 }
+
 function bulletSliderLeft() {
   let indice = 0;
   for (let index = 0; index < nb_points; index++) {
@@ -73,5 +78,5 @@ function bulletSliderLeft() {
   tableau_point[indice].classList.add("dot_selected");
 
   bannerImage.src = "./assets/images/slideshow/" + slides[indice].image;
-  bannerPara.innerHTML = slides[indice].tagLine;
+  bannerText.innerHTML = slides[indice].tagLine;
 }
