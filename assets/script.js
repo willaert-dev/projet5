690ffcc4b78e6d036344abb41fce7683610238bc
willaert-dev <125659755+willaert-dev@users.noreply.github.com>
2023-10-27 12:36:50 +0200

    projet commenté pour la soutenance

--- assets/script.js
+++ assets/script.js
@@ -17,9 +17,11 @@ const slides = [
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
+// récupération et intégration corrspondant aux classes css des flêche 
 const arrow_left = document.querySelector(".arrow_left");
 const arrow_right = document.querySelector(".arrow_right");
 
+// mise en place de l'écoute au click et création de la fonction d'incrémentation et de décrémentation des flèches
 arrow_left.addEventListener("click", function() {
   bulletSlider(-1)
 });
@@ -30,19 +32,16 @@ arrow_right.addEventListener("click", function() {
 function bulletSlider(sliderBullet) {
   console.log(sliderBullet);
 }
-
-const nb_points = slides.length;
-console.log(nb_points)
-
-
 const bannerImage = document.querySelector(".banner-img");
 bannerImage.src ="./assets/images/slideshow/" + slides[0].image;
-
 
 const bannerText = document.querySelector("#banner P");
 bannerText.innerHTML = slides[0].tagLine;
 
-
+//récupération du nombre de bullet point création de la classe dots,
+//  ajout de la classe dots dans le html.
+const nb_points = slides.length;
+console.log(nb_points)
 
 let dots = document.querySelector(".dots");
 for (let i = 0; i < nb_points; i++) {
@@ -50,10 +49,13 @@ for (let i = 0; i < nb_points; i++) {
   dots.appendChild(point);
   point.classList.add("dot");
 }
-
+// sélection et récupération de la classe html dots et et ajout de la classe css, 
+// pour la coloration du point au défilement.
 tableau_point = document.querySelectorAll(".dot");
 tableau_point[0].classList.add("dot_selected");
 
+// rappel de la fonction d'incrémentation et décrementation, pour le déplacement des bullets point
+// d'un point à un autre. 
 function bulletSlider(sliderBullet) {
   let indice = 0;
   for (let index = 0; index < nb_points; index++) {
@@ -69,6 +71,8 @@ function bulletSlider(sliderBullet) {
   } else { indice++};
   tableau_point[indice].classList.add("dot_selected");
 
+// récupération de la source des images et leur intégrations au slide 
+//  gestion du défilement photos et textes correspondant.   
   bannerImage.src = "./assets/images/slideshow/" + slides[indice].image;
   bannerText.innerHTML = slides[indice].tagLine; 
 }
